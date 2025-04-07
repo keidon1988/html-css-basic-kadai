75824ff385a44ae2cefd3634a724e33d7ec37d32
keidon1988 <k-ota@assuloan.com>
2025-04-07 14:52:51 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 体重（kg）と身長（m）を変数に代入
+let weight = 68;     // 体重
+let height = 1.7;    // 身長
+
+// BMIの計算
+let bmi = weight / (height * height);
+
+// コンソールに出力
+console.log("体重：" + weight + "kg");
+console.log("身長：" + height + "m");
+console.log("BMI：" + bmi);
