91e05264cf25fa3e5b2b610c5c8b7f3ea4dd62c4
keidon1988 <k-ota@assuloan.com>
2025-04-15 18:23:38 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const textForm = document.getElementById("text");
+const botan = document.getElementById("btn");
+botan.addEventListener("click", function() {
+    textForm.textContent = "ボタンをクリックしました";
+  });
+  
